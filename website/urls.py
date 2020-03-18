4d1b0dafa59062c653d975b72bce9022ade34946
Shah Wajahat <shahwajahatijaz@gmail.com>
2020-03-18 13:06:59 +0500

    I have completed add web page

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
 path('',views.home,name="home"),
+path('add.html',views.add,name="add"),
+path('sub.html',views.sub,name="sub"),
+path('divide.html',views.divide,name="divide"),
+path('multiply.html',views.multiply,name="multiply"),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,46 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	return render(request,'index.html',{})
+
+def add(request):
+	from random import randint
+
+	num_1=randint(0,9)
+	num_2=randint(0,9)
+
+	if request.method == "POST":
+		answer = request.POST['answer']
+		old_num_1=request.POST['old_num_1']
+		old_num_2=request.POST['old_num_2']
+
+		correct_anser=int(old_num_1) + int(old_num_2)
+		 
+		if int(answer) == correct_anser:
+			my_ans="Correct Answer " + old_num_1 + " + " + old_num_2 + " = " + answer
+			color = "success" 
+			
+		else:
+		 	my_ans="InCorrect Answer " + old_num_1 + " + " + old_num_2 + " is not " + answer + " it is "+ str(correct_anser)
+		 	color =	"danger"
+		return render(request,'add.html',{
+			'answer':answer,
+			'my_ans':my_ans,
+			'num_1':num_1,
+			'num_2':num_2,
+			'color':color
+			})
+		
+	return render(request,'add.html',{
+		'num_1':num_1,
+		'num_2':num_2,
+		})
+	
+
+def sub(request):
+	return render(request,'sub.html',{})
+
+def divide(request):
+	return render(request,'divide.html',{})
+
+def multiply(request):
+	return render(request,'multiply.html',{})
